823434e56a3127bdbc674724f561a70f53de97c2
Shanto76 <44320119+Shanto76@users.noreply.github.com>
2021-07-03 20:24:44 +0600

    Java file

---
+++ src-20210702T191938Z-001/automation/Booking.java
@@ -0,0 +1,58 @@
+package com.automation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Booking extends DriverSetup{
+
+	public static String baseURL = "https://www.phptravels.net/home";
+	
+	
+		
+	@Test
+	public static void PHPTravelsBooking() throws InterruptedException {
+		driver.get(baseURL);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		
+		driver.findElement(By.xpath("//a[contains(@class,'text-center tours')]")).click();
+		driver.findElement(By.xpath("//div[contains(@class,'select2-container form-control hotelsearch locationlisttours')]")).click();
+		driver.findElement(By.xpath("//div[starts-with(text(),'Legoland Malaysia Day Pass')]")).click();
+		driver.findElement(By.xpath("//div[@id='tourtype_chosen']//a[@class='chosen-single']")).click();
+		driver.findElement(By.xpath("//li[normalize-space()='Yacht']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//form[@action='https://www.phptravels.net/tours/search']//div[@id='airDatepickerRange-hotel']//div[@class='form-icon-left']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//body/div[@id='datepickers-container']/div[8]/div[1]/div[1]/div[2]/div[13]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//form[contains(@action,'https://www.phptravels.net/tours/search')]//button[contains(@type,'button')][normalize-space()='+']")).click();
+		driver.findElement(By.xpath("//form[contains(@action,'https://www.phptravels.net/tours/search')]//button[contains(@type,'submit')][normalize-space()='Search']")).click();
+		
+	
+		driver.findElement(By.xpath("/html/body/div[3]/div/div/div[1]/div[4]/div/div[1]/aside/div/form/div/form/button")).click();
+		
+		
+		driver.findElement(By.xpath("//a[@id='guesttab']")).click();
+		driver.findElement(By.xpath("//span[starts-with(text(),'First Name')]")).sendKeys("Smith");
+		driver.findElement(By.xpath("//span[normalize-space()='Last Name']")).sendKeys("San");
+		driver.findElement(By.xpath("//div[@class='col-md-6 col-12 o2']//span[contains(text(),'Email')]")).sendKeys("shakil.shanto76@gmail.com");
+		driver.findElement(By.xpath("//span[normalize-space()='Confirm']")).sendKeys("shakil.shanto76@gmail.com");
+		driver.findElement(By.xpath("//span[normalize-space()='Contact Number']")).sendKeys("01685822476");
+		driver.findElement(By.xpath("//span[normalize-space()='Address']")).sendKeys("Street");
+		
+		driver.findElement(By.xpath("//a[@class='chosen-single']")).click();
+		driver.findElement(By.xpath("//span[normalize-space()='Bangladesh']")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//button[normalize-space()='CONFIRM THIS BOOKING']")).click();
+		
+		
+		
+	}
+	
+}
